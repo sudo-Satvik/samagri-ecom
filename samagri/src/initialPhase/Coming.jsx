12cf104cf69f14fde6c802460ce8ca12b6e8604e
Satvik Sharma <sharmasatvik43@gmail.com>
2024-06-28 22:05:10 +0530

    Fix

--- samagri/src/initialPhase/Coming.jsx
+++ samagri/src/initialPhase/Coming.jsx
@@ -15,7 +15,7 @@ const Coming = () => {
     </div>
     <div className="text-center w-full mt-[70px]">
         <h1 className='text-4xl sm:text-6xl font-medium mt-10 text-[#ff9900] uppercase tracking-widest'>Who we are?</h1>
-        <p className='text-lg sm:mx-0 mx-2 sm:text-center text-justify mt-4 text-gray-600 max-w-screen-xl mx-auto'>
+        <p className='text-lg sm:px-0 px-2 sm:text-center text-justify mt-4 text-gray-600 max-w-screen-xl mx-auto'>
             We are <b>SAMAGRI</b>. SAMAGRI is a one one stop solution for all your religious ceremonies. We are committed to provide you with the most authentic SAMAGRI in the best possible manner.
             The packages has been curated consulting the pandits from culturally significant cities and believed to be best in following the religious rituals.
             Presenting Samagri, the spiritual story of your, mine and our homes.
