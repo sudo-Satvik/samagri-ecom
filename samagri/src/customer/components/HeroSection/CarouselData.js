d20b3378d28835fdc0b1dd5e9a0bc0a70c941099
Satvik Sharma <sharmasatvik43@gmail.com>
2024-07-09 16:34:41 +0530

    Added data in navBar

--- samagri/src/customer/components/HeroSection/CarouselData.js
+++ samagri/src/customer/components/HeroSection/CarouselData.js
@@ -1,6 +1,6 @@
 import firstImage from "../../assets/home/carousel-first.jpg"
 export const CarouselData=[
-
+    
     {
         image: require("../../assets/home/first.png"),
         alt:"Second Image"
